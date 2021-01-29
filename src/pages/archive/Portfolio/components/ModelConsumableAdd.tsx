5fa14acd26ab7f030ea7f3128464b98d9ed066c3
王莉萍 <5556929+wlp6897@user.noreply.gitee.com>
2021-01-29 23:00:44 +0800

    修改日期

--- src/pages/archive/Portfolio/components/ModelConsumableAdd.tsx
+++ src/pages/archive/Portfolio/components/ModelConsumableAdd.tsx
@@ -24,38 +24,48 @@ const ModelConsumableAdd: React.FC<ModalModifyFormDataProps> = ({ createModalVis
   const [ dataSource, setDataSource ] = useState<ColumnEditConsumableType[]>([]);
   const columns: ProColumns<ColumnEditConsumableType>[] = [
     {
-      title: "耗材名称",
+      title: "id",
+      dataIndex: "consumableId",
+      editable: false,
+      width: "40px"
+    },
+    {
+      title: "名称",
       dataIndex: 'consumableName',
       editable: false,
     },
     {
-      title: "耗材编号",
+      title: "编号",
       dataIndex: 'consumableNo',
       editable: false,
+      ellipsis: true
     },
     {
-      title: "耗材类型",
+      title: "类型",
       dataIndex: 'consumableTypeName',
       editable: false,
-      width: "80px"
+      ellipsis: true
     },
     {
-      title: "耗材型号",
+      title: "型号",
       dataIndex: 'consumableModelName',
       editable: false,
-      width: "80px"
+      ellipsis: true
     },
     {
       title: "安装日期",
       dataIndex: 'replacementTime',
-      width: "180px",
-      renderFormItem: (_, { isEditable }) => {
+      width: "170px",
+      renderFormItem: ({ index }, { record, isEditable }) => {
         return isEditable ? <DatePicker
           picker="date"
           format="YYYY-MM-DD"
           placeholder="请选择安装日期"
           allowClear={ false }
           style={ { width: "100%" } }
+          onChange={ (val: any) => {
+            controlExpirationTime(record?.replacementCycle, val, index)
+          } }
         /> : ""
       }
     },
@@ -63,21 +73,23 @@ const ModelConsumableAdd: React.FC<ModalModifyFormDataProps> = ({ createModalVis
       title: "更换周期(天)",
       dataIndex: 'replacementCycle',
       width: "100px",
-      renderFormItem: (_, { isEditable }) => {
-        return isEditable ? <InputNumber style={ { width: "100%" } } min={ 0 } /> : ""
+      renderFormItem: ({ index }, { record, isEditable }) => {
+        return isEditable ? <InputNumber onChange={ (val: any) => {
+          controlExpirationTime(val, record?.replacementTime, index)
+        } } style={ { width: "100%" } } min={ 0 } /> : ""
       }
     },
     {
       title: "到期日期",
       dataIndex: 'expirationTime',
-      width: "180px",
-      renderFormItem: (_, { isEditable }) => {
-        return isEditable ? <DatePicker
-          format="YYYY-MM-DD"
-          placeholder="请选择到期日期"
-          allowClear={ false }
-        /> : ""
-      },
+      width: "170px",
+      valueType: "date",
+      formItemProps: {
+        rules: [ {
+          required: true,
+          message: "请输入到期日期",
+        } ]
+      }
     },
     {
       title: "数量",
@@ -111,6 +123,20 @@ const ModelConsumableAdd: React.FC<ModalModifyFormDataProps> = ({ createModalVis
     handleModalVisible(false);
     queryConsumableList();
   }
+  const controlExpirationTime = async (cycle: any, time: any, index: any) => {
+    let replacementTime = new Date(pickerDateFormat(time)).getTime();
+    let expirationTime = pickerDateFormat(new Date(replacementTime + parseInt(cycle) * 24 * 60 * 60 * 1000))
+    let newDate = dataSource.map((item, idx) => {
+      if (index === idx) {
+        item.replacementCycle = cycle
+        item.expirationTime = expirationTime
+        item.replacementTime = time
+      }
+      return item
+    })
+    await setDataSource([])
+    setDataSource(newDate)
+  }
   return (
     <>
       <Modal
@@ -128,29 +154,33 @@ const ModelConsumableAdd: React.FC<ModalModifyFormDataProps> = ({ createModalVis
           setEditableRowKeys={ setEditableRowKeys }
           portfolioId={ portfolioId }
         />
-        <EditableProTable<ColumnEditConsumableType>
-          rowKey="consumableId"
-          headerTitle="设置新增耗材内容"
-          columns={ columns }
-          value={ dataSource }
-          onChange={ (data) => {
-            setEditableRowKeys(data.map((item) => item.consumableId));
-            setDataSource(data);
-          } }
-          recordCreatorProps={ false }
-          editable={ {
-            type: 'multiple',
-            editableKeys,
-            actionRender: (row: any, config: any, doms: any) => {
-              return [ doms.delete ];
-            },
-            onValuesChange: (record: any, recordList: any) => {
-              setDataSource(recordList);
-            },
-            onChange: setEditableRowKeys
-          } }
-          scroll={ { y: 200 } }
-        />
+        { dataSource.length > 0 &&
+          <EditableProTable<ColumnEditConsumableType>
+            rowKey="consumableId"
+            headerTitle="设置新增耗材内容"
+            columns={ columns }
+            value={ dataSource }
+            onSubmit={ (values) => {
+              console.log(values)
+            } }
+            onChange={ (data) => {
+              setEditableRowKeys(data.map((item) => item.consumableId));
+              setDataSource(data);
+            } }
+            recordCreatorProps={ false }
+            editable={ {
+              type: 'multiple',
+              editableKeys,
+              actionRender: (row: any, config: any, doms: any) => {
+                return [ doms.delete ];
+              },
+              onValuesChange: (record: any, recordList: any) => {
+                setDataSource(recordList);
+              },
+              onChange: setEditableRowKeys,
+            } }
+            scroll={ { y: 200 } }
+          /> }
       </Modal>
     </>
   );
